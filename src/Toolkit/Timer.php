be1e185e8227cea3ee52f3ed72ac7aed39ae83ef
Maarten Thiebou <thiebou@gmail.com>
2022-05-16 01:55:13 +0200

    Update Timer.php

--- src/Toolkit/Timer.php
+++ src/Toolkit/Timer.php
@@ -4,6 +4,11 @@ namespace Modufolio\Toolkit;
 
 class Timer
 {
+    public static function app($decimals = 2): string
+    {
+        $time = microtime(true) - START_TIMER;
+        return number_format($time * 1000, $decimals);
+    }
 
     public static function getExecutionTime($decimals = 2): string
     {
